8901e440f6e4f796748002b192c851b30bdd3609
gergness <greg.freedman@gmail.com>
2019-04-15 17:24:56 -0500

    add ability to scale an sf object

---
+++ R/shift_map.R
@@ -0,0 +1,29 @@
+# Adapted from Laura DeCicco's article Mapping Points
+# (which in turn was based on Bob Rudis's code)
+# http://usgs-r.github.io/dataRetrieval/articles/usMaps.html
+move_sf <- function(sf, scale, shift, rotate = 0, ref = sp) {
+  out <- sf
+  out <- scale_sf(out, scale)
+  out <- shift_sf(out, shift)
+  out <- rotate_sf(out, rotate)
+  out
+}
+
+scale_sf <- function(sf, scale, ref) {
+  geo <- sf::st_geometry(sf)
+  centroid <- sf::st_centroid(sf::st_transform(sf::st_geometry(ref), sf::st_crs(sf)))
+  geo <- ((geo - centroid) * scale) + centroid
+  sf::st_crs(geo) <- sf::st_crs(sf)
+  sf::st_geometry(sf) <- geo
+  sf
+}
+
+shift_sf <- function(sf, shift) {
+  identity(sf)
+}
+
+rotate_sf <- function(sf, rotate) {
+  identity(sf)
+}
+
+
