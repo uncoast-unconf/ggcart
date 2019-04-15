a0c9ea21da9be18f0f0b75e064b3532ba3d64b46
Angela Li <angelali921@gmail.com>
2019-04-15 15:10:51 -0500

    Reversed lat/long (yikes)

--- data-raw/bridges_dams.R
+++ data-raw/bridges_dams.R
@@ -1,13 +1,13 @@
 library(sf)
 
 bridges <- read_sf("data-raw/bridges.csv",
-                   options = c("X_POSSIBLE_NAMES=latitude",
-                               "Y_POSSIBLE_NAMES=longitude"))
+                   options = c("X_POSSIBLE_NAMES=longitude",
+                               "Y_POSSIBLE_NAMES=latitude"))
 bridges <- st_set_crs(bridges, 5070)
 
 dams <- read_sf("data-raw/dams.csv",
-                options = c("X_POSSIBLE_NAMES=latitude",
-                            "Y_POSSIBLE_NAMES=longitude"))
+                options = c("X_POSSIBLE_NAMES=longitude",
+                            "Y_POSSIBLE_NAMES=latitude"))
 dams <- st_set_crs(dams, 5070)
 
 save(bridges, file = "data/bridges.rda")
