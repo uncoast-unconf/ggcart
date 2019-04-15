e792b738e16a1b87c1f675410ab898543f0093e9
Angela Li <angelali921@gmail.com>
2019-04-15 15:34:52 -0500

    4326 is unprojected

--- data-raw/bridges_dams.R
+++ data-raw/bridges_dams.R
@@ -3,12 +3,14 @@ library(sf)
 bridges <- read_sf("data-raw/bridges.csv",
                    options = c("X_POSSIBLE_NAMES=longitude",
                                "Y_POSSIBLE_NAMES=latitude"))
-bridges <- st_set_crs(bridges, 5070)
+bridges <- st_set_crs(bridges, 4326) %>%
+  st_transform(5070)
 
 dams <- read_sf("data-raw/dams.csv",
                 options = c("X_POSSIBLE_NAMES=longitude",
                             "Y_POSSIBLE_NAMES=latitude"))
-dams <- st_set_crs(dams, 5070)
+dams <- st_set_crs(bridges, 4326) %>%
+  st_transform(5070)
 
 save(bridges, file = "data/bridges.rda")
 save(dams, file = "data/dams.rda")
