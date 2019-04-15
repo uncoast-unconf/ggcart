f09b7d8c85f08a81617c1bb6760adbe0a7179091
Charles Minshew <charlespminshew@gmail.com>
2019-04-15 13:59:47 -0500

    Documented data; Updated bridges

---
+++ R/data-dams.R
@@ -0,0 +1,18 @@
+#' Dams
+#'
+#' This data comes from the National Inventory of Dams, <https://nid-test.sec.usace.army.mil/ords/f?p=105:1::::::>
+#'
+#' @format A tibble with 511 rows and 9 variables:
+#' \describe{
+#' \item{city}{The city closest to the dam}
+#' \item{state}{The state the dam is in}
+#' \item{river}{The river that the dam is on}
+#' \item{year_completed}{The year the dam was constructed}
+#' \item{hazard}{The hazard rating of the dam. (H = High; L = Low; S = Signficant; U = Undetermined)}
+#' \item{eap}{Status of the dam's emergency action plan. (Y = Yes; N = No; NR = Not Required;)}
+#' \item{latitude}{The latitude of the dam}
+#' \item{longitude}{The longitude of the dam}
+#' }
+#' @examples
+#' dams
+"dams"
