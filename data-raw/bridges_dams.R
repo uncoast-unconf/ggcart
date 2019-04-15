4be8bd21ea93d0336e746cfe2da0dca0fa9e4cd5
Angela Li <angelali921@gmail.com>
2019-04-15 14:22:43 -0500

    Clean up bridges and dams cleaning code

--- data-raw/bridges_dams.R
+++ data-raw/bridges_dams.R
@@ -1,12 +1,14 @@
 library(sf)
 
 bridges <- read_sf("data-raw/bridges.csv",
-                   options = c("X_POSSIBLE_NAMES=latmap",
-                               "Y_POSSIBLE_NAMES=longmap"))
+                   options = c("X_POSSIBLE_NAMES=latitude",
+                               "Y_POSSIBLE_NAMES=longitude"))
+bridges <- st_set_crs(bridges, 5070)
 
 dams <- read_sf("data-raw/dams.csv",
-                options = c("X_POSSIBLE_NAMES=latmap",
-                            "Y_POSSIBLE_NAMES=longmap"))
+                options = c("X_POSSIBLE_NAMES=latitude",
+                            "Y_POSSIBLE_NAMES=longitude"))
+dams <- st_set_crs(dams, 5070)
 
 save(bridges, file = "data/bridges.rda")
 save(dams, file = "data/dams.rda")
