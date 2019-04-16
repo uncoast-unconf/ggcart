15feb52d5fd5bae3f7c9043f2e05558a2c83b270
gergness <greg.freedman@gmail.com>
2019-04-16 09:38:28 -0500

    Add documentation and rename function (and delete the old functions

--- R/shift_map.R
+++ R/shift_map.R
@@ -2,18 +2,24 @@
 # (which in turn was based on Bob Rudis's code)
 # http://usgs-r.github.io/dataRetrieval/articles/usMaps.html
 
-#' Title
+
+#' Transport (shift, rotate and scale) an sf object
 #'
-#' @param sf
-#' @param ref
-#' @param scale
-#' @param shift
-#' @param rotate
+#' Takes an sf object and a reference sf object and transports
+#' it by scaling, shifting it and rotating it.
 #'
-#' @return
+#' @param sf The sf object to transport
+#' @param ref The reference sf object
+#' @param scale A scaling factor (defaults to 1, meaning no change in scale)
+#' @param shift Distance to shift (TKTKTK what units is it?)
+#' @param rotate Radians to rotate the sf object
+#'
+#' @return An sf object
 #' @export
 #'
 #' @examples
+#' # Transports Puerto Rico off the coast of Georgia, rotate 90 degrees
+#' # made 4 times bigger
 #' ggplot()+
 #' geom_sf(data=albersextra:::lower48)+
 #'   geom_sf(data=albersextra:::puerto_rico)+
@@ -22,7 +28,7 @@
 #'                        scale=4,
 #'                        shift = c(-130,90),
 #'                        rotate=pi/2), color="blue")
-move_sf <- function(sf, ref, scale=1, shift=c(0,0), rotate=0) {
+transport_sf <- function(sf, ref, scale=1, shift=c(0,0), rotate=0) {
   geo <- sf::st_geometry(sf)
   centroid <- sf::st_centroid(sf::st_transform(sf::st_geometry(ref), sf::st_crs(sf)))
   rotation_matrix <- matrix(
@@ -30,47 +36,6 @@ move_sf <- function(sf, ref, scale=1, shift=c(0,0), rotate=0) {
     nrow=2,ncol=2
   )
   geo <- ((((geo - centroid) * scale) * rotation_matrix)+ shift*10000) + centroid
-  sf::st_crs(geo) <- sf::st_crs(sf)
-  sf::st_geometry(sf) <- geo
-  sf
-}
-
-move_sf <- function(sf, scale, shift, rotate = 0, ref = sp) {
-  out <- sf
-  out <- scale_sf(out, scale)
-  out <- shift_sf(out, shift)
-  out <- rotate_sf(out, rotate)
-  out
-}
-
-scale_sf <- function(sf, scale, ref) {
-  geo <- sf::st_geometry(sf)
-  centroid <- sf::st_centroid(sf::st_transform(sf::st_geometry(ref), sf::st_crs(sf)))
-  geo <- ((geo - centroid) * scale) + centroid
-  sf::st_crs(geo) <- sf::st_crs(sf)
-  sf::st_geometry(sf) <- geo
-  sf
-}
-
-shift_sf <- function(sf, shift, ref) {
-  geo <- sf::st_geometry(sf)
-  centroid <- sf::st_centroid(sf::st_transform(sf::st_geometry(ref), sf::st_crs(sf)))
-  geo <- ((geo - centroid) + shift*10000) + centroid
-  sf::st_crs(geo) <- sf::st_crs(sf)
-  sf::st_geometry(sf) <- geo
-  sf
-}
-
-rotate_sf <- function(sf, rotate, ref) {
-  geo <- sf::st_geometry(sf)
-  centroid <- sf::st_centroid(sf::st_transform(sf::st_geometry(ref), sf::st_crs(sf)))
-
-  rotation_matrix <- matrix(
-    c(cos(rotate), sin(rotate), -sin(rotate), cos(rotate)),
-    2,
-    2
-  )
-  geo <- ((geo - centroid) * rotation_matrix) + centroid
   sf::st_crs(geo) <- sf::st_crs(sf)
   sf::st_geometry(sf) <- geo
   sf
