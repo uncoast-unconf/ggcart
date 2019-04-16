664ef0787c2cce02084d604d5a0fa52ba9e9ad49
gergness <greg.freedman@gmail.com>
2019-04-16 13:59:43 -0500

    First pass at geom_sf_albersusa() that kind of works.

--- R/geom_sf_albersusa.R
+++ R/geom_sf_albersusa.R
@@ -11,7 +11,11 @@ GeomSfAlbersusa <- ggplot2::ggproto("GeomSfAlbersusa", ggplot2::Geom,
                   ),
 
                   draw_panel = function(self, data, panel_params, coord, legend = NULL) {
-
+                    bbox <- sf::st_bbox(sf::st_as_sf(data))
+                    # TODO: This is kind of a hack to reset the x and y ranges for new
+                    # data.
+                    panel_params$x_range <- c(bbox$xmin - 100000, bbox$xmax + 100000)
+                    panel_params$y_range <- c(bbox$ymin - 100000, bbox$ymax + 100000)
                     print(nrow(data))
                     print(class(data))
                     continental_usa <- data
@@ -28,6 +32,40 @@ GeomSfAlbersusa <- ggplot2::ggproto("GeomSfAlbersusa", ggplot2::Geom,
                   draw_key =  function(self, data, params) {
                     data <- ggproto_parent(GeomSf, self)$draw_key(self, data, params)
                     data
+                  },
+
+                  setup_data = function(data, params) {
+                    split <- split_map_usa(sf::st_as_sf(data))
+
+                    pr_vi <- sf::st_union(puerto_rico, virgin_islands, by_feature = TRUE)
+
+                    split$puerto_rico <- transport_sf(split$puerto_rico, ref = pr_vi, shift = c(-2500000,20000), scale = 4)
+                    split$virgin_islands <- transport_sf(split$virgin_islands, ref = pr_vi, shift = c(-2500000,20000), scale = 4)
+                    split$hawaii <- transport_sf(split$hawaii, ref = hawaii, shift = c(4680000, -1100000), scale = 1.5, rotate = -0.610865)
+                    split$alaska <- transport_sf(split$alaska, ref = hawaii, shift = c(3510000, -3000000), scale = 0.47, rotate = -0.873)
+
+                    # TODO: Figure out how to deal with GUAM
+                    split$guam <- NULL
+
+                    # TODO: Should there be a warning message when we drop points that
+                    # fall outside our bboxes?
+                    split$unknown <- NULL
+
+                    data <- do.call(rbind, split)
+
+                    data <- sf::st_transform(data, sf::st_crs(bridges))
+                    data <- as.data.frame(data)
+
+                    if (is.null(params$crs))
+                      return(data)
+
+                    lapply(data, function(layer_data) {
+                      if (! is_sf(layer_data)) {
+                        return(layer_data)
+                      }
+
+                      sf::st_transform(layer_data, params$crs)
+                    })
                   }
 
 )
